fac614c864db7fff446f2fb6a652184fe1f533b8
Diego Casagrande <diegocasagrande@hotmail.com>
2021-11-28 21:58:35 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Header } from '../components/Header';
+import { Task, TasksList } from '../components/TasksList';
+import { TodoInput } from '../components/TodoInput';
+
+export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleAddTask(newTaskTitle: string) {
+    setTasks([...tasks, { id: new Date().getTime(), title: newTaskTitle, done: false }]);
+  }
+
+  function handleToggleTaskDone(id: number) {
+    setTasks(tasks.map(task => task.id === id ? { ...task, done: !task.done } : task));
+  }
+
+  function handleRemoveTask(id: number) {
+    console.log("id removido: ", id);
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header tasksCounter={tasks.length} />
+
+      <TodoInput addTask={handleAddTask} />
+
+      <TasksList 
+        tasks={tasks} 
+        toggleTaskDone={handleToggleTaskDone}
+        removeTask={handleRemoveTask} 
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBEBEB'
+  }
+})
